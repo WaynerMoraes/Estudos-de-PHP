5a3bbed5e86bf2412d6656828218ea770199124a
wayner <wayner.junior1995@gmail.com>
2024-03-06 21:03:53 -0300

    Primeiro commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+
+$host = "localhost";
+$port = 3306;
+$user = "root";
+$pass = "";
+$dbname = "wayner";
+
+try {
+
+    $conn = new PDO("mysql:host=$host;port=$port;dbname=". $dbname, $user, $pass);
+
+    var_dump($conn);
+
+    echo"Conexão realizada com sucesso!";
+
+}catch(PDOException $e) {
+
+    echo "Erro de conexão com o banco de dados!</br> Erro:". $e->getMessage();
+}
+
